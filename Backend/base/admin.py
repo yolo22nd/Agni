abe456b8597f7f6a820990c26193900bebb30d50
Kaumudi Soman <kaumudi814@gmail.com>
2024-02-24 16:59:27 +0530

    added models

--- Backend/base/admin.py
+++ Backend/base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+class CommitteeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'department', 'desc'] 
+
+admin.site.register(Committee, CommitteeAdmin)
