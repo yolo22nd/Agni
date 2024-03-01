64b01215a8cd19192aa3cb54abd76db515b99c33
milanh34 <milanh344@gmail.com>
2024-03-01 16:06:35 +0530

    Improved Approval Status feature

--- frontend/src/pages/Committee.js
+++ frontend/src/pages/Committee.js
@@ -9,6 +9,8 @@ import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 function Committee() {
   const [render, setRender] = useState(false);
@@ -256,27 +258,20 @@ function Committee() {
             <ol class="flex items-center w-full text-sm font-medium text-center text-gray-100 sm:text-base">
               <li class="flex md:w-full items-center text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b  after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 after:border-gray-700">
                 <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-500">
-                  {currentEvent.is_pending? "" : <svg
-                    class="w-5 h-5 sm:w-8 sm:h-8 me-2.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                >
-                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                </svg>}
-                  <span class="hidden sm:inline-flex sm:ms-2">{currentEvent.is_pending? "Pending" : "Passed Forward"}</span>
+                {(!currentEvent.is_approved  && !currentEvent.is_rejected )? "" : <CheckCircleIcon/>}
+                  <span class="hidden sm:inline-flex sm:ms-2">{(!currentEvent.is_approved  && !currentEvent.is_rejected )? "Pending" : "Passed Forward"}</span>
                 </span>
               </li>
               <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
                 <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-500">
+                {!currentEvent.is_approved? (!currentEvent.is_rejected? "" : <CancelIcon/>) : <CheckCircleIcon/>}
                   <span class="hidden sm:inline-flex sm:ms-2">
-                  {currentEvent.is_pending? "Approval Status" : (currentEvent.is_approved? "Approved" : "Rejected")}
+                  {!currentEvent.is_approved? (!currentEvent.is_rejected? "Pending" : "Rejected") : "Approved"}
                   </span>
                 </span>
               </li>
               <li class="flex items-center">
-                <span class="me-2">{currentEvent.is_pending? "Approval Status" : (currentEvent.is_approved? currentEvent.date : "-")}</span>
+                <span class="me-2">{!currentEvent.is_approved? (!currentEvent.is_rejected? "Pending" : "-") : currentEvent.date}</span>
               </li>
             </ol>
           </div>
